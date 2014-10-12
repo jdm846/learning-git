7cceed01ffb2770d98d45a14417ef695bd49935a
bghmmbp01 admin <admin@bghmmbp01.com>
2014-10-12 22:29:22 +0530

    Initial Commit

---
+++ SingleViewApplication3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleViewApplication3
+//
+//  Created by administrator on 10/12/14.
+//  Copyright (c) 2014 aricent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SingleViewApplication3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SingleViewApplication3
+//
+//  Created by administrator on 10/12/14.
+//  Copyright (c) 2014 aricent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
